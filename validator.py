1adf95ecabbf5d87e00c1a6c3eeb1ead4d867393
Johan Lundahl <johan.t.lundahl@gmail.com>
2018-08-09 16:20:38 +0200

    Update validator.py

--- validator.py
+++ validator.py
@@ -2,16 +2,28 @@ class Rules:
     def __init__(self):
         self._rules = []
     
-    def add_rule(self, func):
-        self._rules.append(func)
+    def add_rule(self, func, feedback = None):
+        self._rules.append(Rule(func, feedback))
     
     def validate(self, obj):
         try:
-            return all(rule(obj) for rule in self._rules)
+            return all(rule.func(obj) for rule in self._rules)
         except:
             return False
-    
 
+class Rule:
+    def __init__(self, func, feedback):
+        self._func = func
+        self._feedback = feedback
+    
+    @property
+    def feedback(self):
+        return self._feedback
+    
+    @property
+    def func(self):
+        return self._func
+    
 if __name__ == '__main__':    
     # Example on how to use Rules to validate passwords
     rules = Rules()
