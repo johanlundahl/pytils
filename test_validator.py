951b2c9f9d1509f0eac2801c4d447982f9440533
Johan Lundahl <johan.t.lundahl@gmail.com>
2018-12-28 20:06:51 +0100

    Add tests for validator

---
+++ test_validator.py
@@ -0,0 +1,50 @@
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+import unittest
+from validator import Checker
+
+class Search(unittest.TestCase):
+    def setUp(self):
+        pass
+    
+    def test_validate_single_rule(self):
+        rule = Checker()
+        rule.add_rule(lambda s: any(x.isdigit() for x in s))
+        self.assertTrue(rule.validate('123'))
+        self.assertFalse(rule.validate('abc'))
+    
+    def test_validate_all_of_several_rules(self):
+        rule = Checker().all()
+        rule.add_rule(lambda s: any(x.isdigit() for x in s))
+        rule.add_rule(lambda s: any(x.islower() for x in s))
+        rule.add_rule(lambda s: any(x.isupper() for x in s))
+        
+        self.assertFalse(rule.validate('123'))
+        self.assertFalse(rule.validate('1ab'))
+        self.assertTrue(rule.validate('1Ab'))
+    
+    def test_validate_any_of_several_rules(self):
+        rules = Checker()
+        rules.add_rule(lambda s: any(x.isdigit() for x in s))
+        rules.add_rule(lambda s: any(x.islower() for x in s))
+        rules.add_rule(lambda s: any(x.isupper() for x in s))
+        
+        self.assertTrue(rules.any().validate('abc'))
+        self.assertFalse(rules.any().validate('...'))
+        self.assertTrue(rules.any().validate('1'))
+        
+    def test_evaluate_to_message(self):
+        age = Checker().all()
+        age.add_rule(lambda i: i >= 0, 'Must be positive int')
+        age.add_rule(lambda i: i < 130, 'No one is that old')
+        
+        self.assertEqual(age.evaluate(-1), 'Must be positive int')
+        self.assertEqual(age.evaluate(1), None)
+        
+        
+    def tearDown(self):
+        pass
+    
+if __name__ == '__main__':
+    unittest.main()
