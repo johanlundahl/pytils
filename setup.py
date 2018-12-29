8a26f9138e0f51f7efe355a00abcef101a6fde7d
Lundahl <lundahl@Lundahls-MacBook-Air.local>
2018-12-29 20:40:07 +0100

    Updating package names

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='pytils',
     version='0.1',
-    packages=['pytils',],
+    packages=['',],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.txt').read(),
 )
