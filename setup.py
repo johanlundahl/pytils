28d08825e407cd6f812cfa146ca70aba2c0c2e72
Johan Lundahl <johan.t.lundahl@gmail.com>
2019-08-07 19:57:08 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='pytils',
     version='0.2',
-    packages=find_packages(),
+    packages=find_packages(include=['pytils', 'pytils.*']),
     scripts=['validator.py'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.md').read(),
