2c55d9ccca126ec0f0368465a629509edb727b0d
Johan Lundahl <johan.t.lundahl@gmail.com>
2019-08-07 20:08:50 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='pytils',
     version='0.3',
-    packages=find_packages(include=['pytils', 'pytils.*']),
+    py_modules = ['validator'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     url='https://github.com/johanlundahl/pytils'
 )
