9e2a24fc49b338570bbd5982d931046f214f61ea
Lundahl <lundahl@Lundahls-MacBook-Air.local>
2019-08-07 20:31:38 +0200

    Increased version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pytils',
-    version='0.3',
+    version='0.4',
     packages = ['pytils'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     url='https://github.com/johanlundahl/pytils'
