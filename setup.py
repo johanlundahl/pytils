f3a8f123a1e5bf8a3e44cc518cc82103872eccf6
Lundahl <lundahl@Lundahls-MacBook-Air.local>
2019-08-07 20:25:29 +0200

    Improved package structure for easier setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='pytils',
     version='0.3',
-    py_modules = ['validator'],
+    packages = ['pytils'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     url='https://github.com/johanlundahl/pytils'
 )
