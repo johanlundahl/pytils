f1b619c5a192923be97fdd261881102dfb9bf5c8
Johan Lundahl <johan.t.lundahl@gmail.com>
2019-08-07 20:01:22 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,6 @@ setup(
     name='pytils',
     version='0.3',
     packages=find_packages(include=['pytils', 'pytils.*']),
-    scripts=['validator.py'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
-    long_description=open('README.md').read(),
-    long_description_content_typ='text/markdown',
     url='https://github.com/johanlundahl/pytils'
 )
