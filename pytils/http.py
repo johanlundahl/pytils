9aaeadc244223048a3ab4eaed339f689c101956e
Lundahl <lundahl@Lundahls-MacBook-Air.local>
2019-11-24 18:36:12 +0100

    corrected indents

--- pytils/http.py
+++ pytils/http.py
@@ -2,11 +2,11 @@ import requests
 import json
 
 def post_json(url, obj):
-	headers = {'content-type': 'application/json'}
-	response = requests.post(url, data = obj, headers = headers)
-	return response.status_code, response.text
+    headers = {'content-type': 'application/json'}
+    response = requests.post(url, data = obj, headers = headers)
+    return response.status_code, response.text
 
 def get_json(url):
-	headers = {'content-type': 'application/json'}
+    headers = {'content-type': 'application/json'}
     response = requests.get(url, headers = headers)
     return response.status_code, response.json()
