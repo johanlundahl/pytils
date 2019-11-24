742818e3fa1c5b220e4150f72e786242ce9e11bb
Lundahl <lundahl@Lundahls-MacBook-Air.local>
2019-11-24 18:49:18 +0100

    added slack module

---
+++ pytils/slack.py
@@ -0,0 +1,8 @@
+import requests
+
+def post(webhook_url, message, image_url = None):
+    payload = {"text": "{}".format(message)}
+    if image_url is not None:
+        payload['attachments'] = [{'fallback':'Overview of this weeks business.', 'image_url': image_url}]
+    r = requests.post(url = webhook_url, json = payload)
+    return r.status_code, r.text
