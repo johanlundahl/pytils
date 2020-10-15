e6253b6093b4928a73b92267d1983888945bce15
Lundahl <lundahl@Lundahls-MacBook-Air.local>
2020-10-15 21:07:21 +0200

    merged two modules into logger

--- pytils/logger.py
+++ pytils/logger.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+import time
 import logging
 
 def init(log_name = 'application'):
@@ -18,3 +20,19 @@ def error(msg):
 
 def exception(msg):
 	logging.exception(msg)
+
+def elapsed_time(func):
+  def wrapper(*args, **kwargs):
+    start = datetime.now()
+    res = func(*args, **kwargs)
+    elapsed = datetime.now() - start
+    print('{} elapsed in {}'.format(func.__name__, elapsed))
+    return res
+  return wrapper
+
+def called_at(func):
+  def wrapper(*args, **kwargs):
+    start = datetime.now()
+    print('{} {}{}'.format(start, func.__name__, args))
+    return func(*args, **kwargs)
+  return wrapper
