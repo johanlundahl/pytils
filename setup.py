2df845d54d6e154696c9a3e90cefb7f846e08311
Johan Lundahl <johan.t.lundahl@gmail.com>
2020-10-28 18:22:44 +0100

    new version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pytils',
-    version='0.992',
+    version='0.993',
     packages = ['pytils'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     url='https://github.com/johanlundahl/pytils'
