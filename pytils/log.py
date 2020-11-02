6660328c8763fd4ef9108f4e1981a3fcade5276f
Johan Lundahl <johan.t.lundahl@gmail.com>
2020-11-02 21:26:59 +0100

    corrected indents

--- pytils/log.py
+++ pytils/log.py
@@ -3,11 +3,11 @@ import time
 import logging
 
 def init(log_name = 'application'):
-	file_name = '{}.log'.format(log_name)
-	msg_format = '%(asctime)s - %(levelname)s - %(message)s'
-	date_format = '%Y-%m-%d %H:%M:%S'
+    file_name = '{}.log'.format(log_name)
+    msg_format = '%(asctime)s - %(levelname)s - %(message)s'
+    date_format = '%Y-%m-%d %H:%M:%S'
 
-	logging.basicConfig(filename=file_name, filemode='w', format=msg_format, datefmt=date_format, level=logging.INFO)
+    logging.basicConfig(filename=file_name, filemode='w', format=msg_format, datefmt=date_format, level=logging.INFO)
 
 def info(msg):
     logging.info(msg)
@@ -19,20 +19,20 @@ def error(msg):
     logging.error(msg)
 
 def exception(msg):
-	logging.exception(msg)
+    logging.exception(msg)
 
 def elapsed_time(func):
-  def wrapper(*args, **kwargs):
-    start = datetime.now()
-    res = func(*args, **kwargs)
-    elapsed = datetime.now() - start
-    print('{} elapsed in {}'.format(func.__name__, elapsed))
-    return res
-  return wrapper
+    def wrapper(*args, **kwargs):
+        start = datetime.now()
+        res = func(*args, **kwargs)
+        elapsed = datetime.now() - start
+        print('{} elapsed in {}'.format(func.__name__, elapsed))
+        return res
+    return wrapper
 
 def called_at(func):
-  def wrapper(*args, **kwargs):
-    start = datetime.now()
-    print('{} {}{}'.format(start, func.__name__, args))
-    return func(*args, **kwargs)
-  return wrapper
+    def wrapper(*args, **kwargs):
+        start = datetime.now()
+        print('{} {}{}'.format(start, func.__name__, args))
+        return func(*args, **kwargs)
+    return wrapper
