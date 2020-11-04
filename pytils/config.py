e550b787be864d182d4a31457d4d0b3cacf76cd5
Johan Lundahl <johan.t.lundahl@gmail.com>
2020-11-04 21:35:35 +0100

    improved load of config yaml

--- pytils/config.py
+++ pytils/config.py
@@ -2,10 +2,14 @@ import yaml
 from box import Box
 import sys
 
-def load_config():
-    app_name = sys.modules['__main__'].__file__.split('.')[0]
-    config_file = '{}.yaml'.format(app_name) 
+def load_config(file_name=None):
+	config_file = file_name if file_name != None else get_main_name()
     with open(config_file, 'r') as stream:
         return yaml.safe_load(stream)
+
+def get_main_name():
+	app_name = sys.modules['__main__'].__file__.split('.')[0]
+    return '{}.yaml'.format(app_name) 
+   	
         
 cfg = Box(load_config())
