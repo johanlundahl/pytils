9238f4828cc25b7a8708c304cd6ac0a9da22d336
Johan Lundahl <johan.t.lundahl@gmail.com>
2020-11-06 20:35:56 +0100

    corrected indent

--- pytils/config.py
+++ pytils/config.py
@@ -3,23 +3,23 @@ from box import Box
 import sys
 
 def load_config(file_name=None):
-	config_file = file_name if file_name != None else get_main_name()
+    config_file = file_name if file_name != None else get_main_name()
     with open(config_file, 'r') as stream:
         return yaml.safe_load(stream)
 
 def has_main():
-	try:
-		get_main_name()
-		return True
-	except:
-		return False
+    try:
+        get_main_name()
+        return True
+    except:
+        return False
 
 def get_main_name():
-	app_name = sys.modules['__main__'].__file__.split('.')[0]
+    app_name = sys.modules['__main__'].__file__.split('.')[0]
     return '{}.yaml'.format(app_name) 
 
 def init(file_name=None):
-   	cfg = Box(load_config(file_name))
+    cfg = Box(load_config(file_name))
 
 if has_main():
-	init()
+    init()
