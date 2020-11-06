4dc344bea39e77b3fc1eb8171afebd065007ddff
Johan Lundahl <johan.t.lundahl@gmail.com>
2020-11-06 18:28:32 +0100

    updated to enable for apache

--- pytils/config.py
+++ pytils/config.py
@@ -7,9 +7,19 @@ def load_config(file_name=None):
     with open(config_file, 'r') as stream:
         return yaml.safe_load(stream)
 
+def has_main():
+	try:
+		get_main_name()
+		return True
+	except:
+		return False
+
 def get_main_name():
 	app_name = sys.modules['__main__'].__file__.split('.')[0]
     return '{}.yaml'.format(app_name) 
-   	
-        
-cfg = Box(load_config())
+
+def init(file_name=None):
+   	cfg = Box(load_config(file_name))
+
+if has_main():
+	init()
