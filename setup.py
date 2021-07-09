44098eaccd777b95b7cfc065bc53f30c255d584b
Johan Lundahl <johan.t.lundahl@gmail.com>
2021-07-09 11:31:42 +0200

    moved modules from requirements to setup

--- setup.py
+++ setup.py
@@ -3,8 +3,16 @@ from setuptools import setup
 
 setup(
     name='pytils',
-    version='1.0.0',
+    version='1.0.1',
     packages=['pytils'],
+    include_package_data=True,
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
-    url='https://github.com/johanlundahl/pytils'
+    url='https://github.com/johanlundahl/pytils',
+    install_requires=[
+        'argparse',
+        'python-box',
+        'pyyaml',
+        'requests',
+        'flask'
+    ]
 )
