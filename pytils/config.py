ab04b223749153d3b3ead4976638b2ba83c1f4d1
Johan Lundahl <johan.t.lundahl@gmail.com>
2022-11-25 12:48:43 +0100

    base class to generate config from dataclasses

--- pytils/config.py
+++ pytils/config.py
@@ -1,14 +1,21 @@
+import os.path
+import abc
+from dataclasses import asdict
 import yaml
 from box import Box
+from os import path
 import sys
 
 
+
+# deprecate
 def load_config(file_name=None):
     config_file = file_name if file_name is not None else get_main_name()
     with open(config_file, 'r') as stream:
         return yaml.safe_load(stream)
 
 
+# deprecate
 def has_main():
     try:
         get_main_name()
@@ -22,5 +29,30 @@ def get_main_name():
     return '{}.yaml'.format(app_name)
 
 
+# deprecate
 def init(file_name=None):
     return Box(load_config(file_name))
+
+
+class Configuration(metaclass=abc.ABCMeta):
+
+    @classmethod
+    def init(cls, file_name=None):
+        file_name = file_name if file_name is not None else get_main_name()
+        if path.exists(file_name):
+            config = cls.load(file_name)
+            return config
+        else:
+            config = cls()
+            config.save(file_name)
+            return config
+
+    @classmethod
+    def load(cls, file_name):
+        with open(file_name) as stream:
+            params = yaml.safe_load(stream)
+            return Box(params)
+
+    def save(self, file_name):
+        with open(file_name, 'w+') as file:
+            file.write(yaml.dump(asdict(self)))
---
+++ tests/config_test.py
@@ -0,0 +1,30 @@
+import unittest
+from unittest.mock import patch, mock_open
+from dataclasses import dataclass
+from io import StringIO
+from pytils.config import Configuration
+
+
+class ConfigurationTest(unittest.TestCase):
+
+    @patch('pytils.config.path.exists')
+    @patch('builtins.open', mock_open(read_data='name: Nobody'))
+    def test_init_file_exists(self, mock_exists):
+        mock_exists.return_value = True
+        config = Config.init()
+        self.assertEqual(config.name, 'Nobody')
+
+
+    @patch('pytils.config.path.exists')
+    @patch('builtins.open', new_callable=StringIO)
+    @unittest.skip("reason for skipping")
+    def test_init_file_dont_exists(self, mock_open, mock_exists):
+        mock_exists.return_value = False
+        #mock_open.return_value.__enter__ = StringIO('')
+        config = Config.init()
+        self.assertEqual(config.name, 'Nobody')
+
+
+class Config(Configuration):
+    name: str = 'Someone'
+    age: int = 42
