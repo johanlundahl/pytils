361adab0dfe643545429f63cc2cc4899b4ed0674
Johan Lundahl <johan.t.lundahl@gmail.com>
2022-12-04 20:11:42 +0100

    removed old config handling logic

--- pytils/config.py
+++ pytils/config.py
@@ -6,30 +6,9 @@ from os import path
 import sys
 
 
-# deprecate
-def load_config(file_name=None):
-    config_file = file_name if file_name is not None else get_main_name()
-    with open(config_file, 'r') as stream:
-        return yaml.safe_load(stream)
-
-
-# deprecate
-def has_main():
-    try:
-        get_main_name()
-        return True
-    except KeyError:
-        return False
-
-
 def get_main_name():
     app_name = sys.modules['__main__'].__file__.split('.')[0]
     return '{}.yaml'.format(app_name)
-
-
-# deprecate
-def init(file_name=None):
-    return Box(load_config(file_name))
 
 
 class Configuration(metaclass=abc.ABCMeta):
